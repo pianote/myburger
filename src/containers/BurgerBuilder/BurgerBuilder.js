d5c411cb0215c4ac560520c43d329a2ce344a064
Duc Vo <pianote@gmail.com>
2020-05-29 18:37:46 -0700

    testing units

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,7 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {
