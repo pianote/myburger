b633269e0128f3fa87b2ae9a46ad4bebc3661ea9
Duc Vo <pianote@gmail.com>
2020-06-01 04:09:06 -0700

    add missing dependency

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ const Auth = React.lazy(() => {
 
 const App = props => {
 
+  const { onTryAutoSignup } = props;
   useEffect(()=>{
-    props.onTryAutoSignup()
-  },[props]);
+    onTryAutoSignup()
+  },[onTryAutoSignup]);
   
   let routes = (
     <Switch>
